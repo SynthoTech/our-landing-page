dce496a8b95d704ab9fbc2a1d2d38477457bc615
Abdul <abdulhafis2847@gmail.com>
2023-08-10 12:23:56 +0000

    An Api Endpoint for service that perform the CRUD Operation

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+const ejs = require('ejs');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path')
 
 dotenv.config();
 
-const connectionString = process.env.MONGODB_URL;
+const connectionString = process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/testproject';
 const PORT = process.env.PORT || 3030;
 
 function dbConnect() {
@@ -25,12 +28,26 @@ dbConnect();
 
 const app = express();
 
+// Set up view engine
+app.set('views', path.join(__dirname, '../views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Using middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
 
 // Routes
 const applyRoutes = require('./routes/applyRoutes');
+const serviceRoutes = require('./routes/serviceRoutes');
 app.use('/api/apply', applyRoutes)
+app.use('/api/service', serviceRoutes)
 
 
 
