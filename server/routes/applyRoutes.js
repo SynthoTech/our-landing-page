02a65f0ba5eeedb9c430db89f741dcdfb7d34058
Yussif Yahuza <yussifyahuza12@gmail.com>
2023-08-14 23:41:56 +0000

    Endpoints for contact us and some updates

--- server/routes/applyRoutes.js
+++ server/routes/applyRoutes.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const applyController = require('../controllers/applyController');
+const express = require("express");
+const applyController = require("../controllers/applyController");
 
 const router = express.Router();
 
-router.post('/', applyController.createApplication);
-router.get('/', applyController.getAllApplications);
+router.post("/create", applyController.createApplication);
+router.get("/", applyController.getAllApplications);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,27 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser')
-const ejs = require('ejs');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const path = require('path')
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const path = require("path");
 
 dotenv.config();
 
-const connectionString = process.env.MONGODB_URL ;
+const connectionString = process.env.MONGODB_URL;
 const PORT = process.env.PORT || 3030;
 
 function dbConnect() {
-    mongoose.connect(connectionString, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      })
-        .then(() => {
-          console.log('Connected to MongoDB Atlas');
-        })
-        .catch(err => {
-          console.error('Error connecting to MongoDB Atlas:', err);
-        });
+  mongoose
+    .connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB:", err);
+    });
 }
 
 dbConnect();
@@ -29,9 +30,9 @@ dbConnect();
 const app = express();
 
 // Set up view engine
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, '../public')));
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "../public")));
 
 // Using middlewares
 app.use(express.json());
@@ -44,13 +45,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 // Routes
-const applyRoutes = require('./routes/applyRoutes');
-const serviceRoutes = require('./routes/serviceRoutes');
-app.use('/api/apply', applyRoutes)
-app.use('/api/service', serviceRoutes)
+const applyRoutes = require("./routes/applyRoutes");
+const serviceRoutes = require("./routes/serviceRoutes");
+const contactRoutes = require("./routes/contactRoutes");
+app.use("/api/apply", applyRoutes);
+app.use("/api/service", serviceRoutes);
+app.use("/api/contact", contactRoutes);
 
-
-
-app.listen(PORT,
-    console.log(`Server is running on http://localhost:${PORT}`)
-);
+app.listen(PORT, console.log(`Server is running on http://localhost:${PORT}`));
