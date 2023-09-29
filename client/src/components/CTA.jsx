3d4db44f4aeebbaa0fb5a952df69c4e60d7d3806
Yussif Yahuza <yussifyahuza12@gmail.com>
2023-09-29 18:12:17 +0000

    Minor fixes

--- client/src/components/CTA.jsx
+++ client/src/components/CTA.jsx
@@ -6,12 +6,16 @@ const CTA = () => {
     <section id="cta" className="cta">
       <div className="flex lg:flex-row flex-col max-lg:flex-col  xs:flex-col lg:justify-between items-center px-2 ">
         <div className="sm:text-left text-center">
-          <h3 className="text-white font-black text-[28px]">Call To Action</h3>
+          <h3 className="text-white font-black text-[28px]">
+            Experience Seamless Efficiency and Growth
+          </h3>
           <p className="text-white font-light">
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur.
-            <br /> Excepteur sint occaecat cupidatat non proident, sunt in culpa
-            qui officia deserunt mollit anim id est laborum.
+            Ready to take your business to the next level? Our cutting-edge
+            products & services is designed to supercharge your operations,
+            streamline processes, and boost productivity.
+            <br />
+            Join thousands of satisfied users who are already reaping the
+            benefits.
           </p>
         </div>
         <div className="text-center mt-5">
@@ -19,7 +23,7 @@ const CTA = () => {
             href="#"
             className="text-center text-white font-[500px] sm:py-[16px] sm:px-[40px] py-[15px] px-[30px] hover:bg-[#47b2e4] hover:border-[#47b2e4] rounded-[50px] transition-[0.5s] m-[10px] border-[2px] border-solid border-white"
           >
-            Call To Action
+            Get Started Now
           </a>
         </div>
       </div>
